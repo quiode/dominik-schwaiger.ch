4ac51d273ee9a7a0e82cd74724b7b306ab6977b7
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2022-05-16 12:44:39 +0200

    maturaarbeit in documents

--- pages/documents.vue
+++ pages/documents.vue
@@ -1,30 +1,30 @@
 <script lang="ts" setup>
-  onMounted(() => {
-    // if fill-content is included in classList, wait a little bit
-    // to make sure it's done
+onMounted(() => {
+  // if fill-content is included in classList, wait a little bit
+  // to make sure it's done
 
-    let delay = 400;
+  let delay = 400;
 
-    if (document.documentElement.classList.contains('fill-content')) {
-      delay = 450;
-    }
+  if (document.documentElement.classList.contains('fill-content')) {
+    delay = 450;
+  }
 
-    setTimeout(() => {
-      document.documentElement.classList.add('fill-content');
-    }, delay);
-  });
+  setTimeout(() => {
+    document.documentElement.classList.add('fill-content');
+  }, delay);
+});
 
-  onUnmounted(() => {
-    setTimeout(() => {
-      document.documentElement.classList.remove('fill-content');
-    }, 400);
-  });
+onUnmounted(() => {
+  setTimeout(() => {
+    document.documentElement.classList.remove('fill-content');
+  }, 400);
+});
 
-  const documents: {
-    name: string;
-    icon: string;
-    path: string;
-  }[] = [
+const documents: {
+  name: string;
+  icon: string;
+  path: string;
+}[] = [
     {
       name: 'Curriculum Vitae',
       icon: 'bi-file-earmark-person',
@@ -35,26 +35,23 @@
       icon: 'bi-fingerprint',
       path: 'gpg.asc',
     },
+    {
+      name: 'Final Thesis',
+      icon: 'bi-file-earmark-pdf',
+      path: 'Schwaiger - Bau eines 8-Bit Prozessors.pdf'
+    }
   ];
 </script>
 
 <template>
   <div>
     <div id="container">
-      <a
-        :href="'/' + document.path"
-        :download="document.path"
-        target="_blank"
-        v-for="document in documents"
-        class="item-container"
-      >
-        <div
-          class="icon"
-          :style="{
-            animationDuration: Math.random() + 2 + 's',
-            animationDelay: Math.random() + 's',
-          }"
-        >
+      <a :href="'/' + document.path" :download="document.path" target="_blank" v-for="document in documents"
+        class="item-container">
+        <div class="icon" :style="{
+          animationDuration: Math.random() + 2 + 's',
+          animationDelay: Math.random() + 's',
+        }">
           <i class="bi" :class="document.icon"></i>
         </div>
 
@@ -65,70 +62,72 @@
 </template>
 
 <style lang="scss" scoped>
-  @import '../assets/colors.scss';
+@import '../assets/colors.scss';
 
-  #container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    align-items: center;
-    align-content: center;
+#container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  align-content: center;
 
-    .item-container {
-      cursor: pointer;
-      border-color: $secondary-color;
-      border-radius: 2em;
-      padding: 5em;
-      margin: 5em;
-      border-style: solid;
+  .item-container {
+    cursor: pointer;
+    border-color: $secondary-color;
+    border-radius: 2em;
+    padding: 5em;
+    margin: 5em;
+    border-style: solid;
 
-      &:hover {
-        @keyframes hovering {
-          $rotation: 5deg;
+    &:hover {
+      @keyframes hovering {
+        $rotation: 5deg;
 
-          0% {
-            transform: rotate($rotation);
-          }
-
-          50% {
-            transform: rotate(-$rotation);
-          }
-
-          100% {
-            transform: rotate($rotation);
-          }
+        0% {
+          transform: rotate($rotation);
         }
 
-        animation: hovering 0.5s infinite;
-      }
-
-      .icon {
-        @keyframes gradient {
-          0% {
-            background-position: 0% 50%;
-          }
-          50% {
-            background-position: 100% 50%;
-          }
-          100% {
-            background-position: 0% 50%;
-          }
+        50% {
+          transform: rotate(-$rotation);
         }
 
-        font-size: 10em;
-        text-align: center;
-        background: $gradient-light-top-right;
-        background-clip: text;
-        -webkit-text-fill-color: transparent;
-        animation: gradient 3s ease infinite;
-        background-size: 400% 400%;
+        100% {
+          transform: rotate($rotation);
+        }
       }
 
-      .name {
-        text-align: center;
-        font-size: 2em;
-        word-break: break-all;
+      animation: hovering 0.5s infinite;
+    }
+
+    .icon {
+      @keyframes gradient {
+        0% {
+          background-position: 0% 50%;
+        }
+
+        50% {
+          background-position: 100% 50%;
+        }
+
+        100% {
+          background-position: 0% 50%;
+        }
       }
+
+      font-size: 10em;
+      text-align: center;
+      background: $gradient-light-top-right;
+      background-clip: text;
+      -webkit-text-fill-color: transparent;
+      animation: gradient 3s ease infinite;
+      background-size: 400% 400%;
+    }
+
+    .name {
+      text-align: center;
+      font-size: 2em;
+      word-break: break-all;
     }
   }
+}
 </style>
