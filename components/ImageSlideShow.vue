9b7796b1103294c70baf9379715bdd50f382932b
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-08-20 01:00:55 +0200

    slideshow basics

---
+++ components/ImageSlideShow.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import type { ImageFile } from '../server/api/images.post';
+
+defineProps<{
+  images: ImageFile[],
+  index: number
+}>();
+
+const emit = defineEmits(['close']);
+
+onMounted(() => {
+  window.addEventListener('keydown', onKeyDown);
+});
+onBeforeUnmount(() => {
+  window.removeEventListener('keydown', onKeyDown);
+});
+
+function onKeyDown(event: KeyboardEvent) {
+  if (event.key == 'Escape') {
+    emit('close');
+  }
+};
+</script>
+
+<template>
+</template>
+
+<style lang="scss" scoped></style>
