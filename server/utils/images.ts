869a38e89279f37f6d0bf8eed362c9c0d8e1def8
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-08-22 00:37:51 +0200

    hash images as to avoid duplicates

--- server/utils/images.ts
+++ server/utils/images.ts
@@ -1,38 +1,44 @@
 import { v4 as uuidv4 } from 'uuid';
 import sharp from 'sharp';
 import { writeFile, readFile, access, mkdir, rm } from 'fs/promises';
+import { hash } from 'crypto';
 
 // TODO: concurrency problems could occur with json if one thread safes an old value. Should not occur if one normally only uploads and deletes not concurrent, but synchronization would be beneficial
 
 export async function uploadImage(binaryString: Buffer) {
-  const fileName = uuidv4();
+  const hash = hashBuffer(binaryString);
 
-  // get metadata
-  // convert image
-  const data1 = await sharp(binaryString)
-    .webp({ quality: 90 })
-    .toFile(full_size + fileName + ".webp");
-  // create a thumbnail
-  const data2 = await sharp(binaryString)
-    .resize({ width: 1920, withoutEnlargement: true })
-    .webp({ quality: 60 })
-    .toFile(thumbnails + fileName + ".webp");
-  // add file to file index
-  const json: ImageFile[] = JSON.parse(await readFile(json_file, "utf8"));
-  const newJSON: ImageFile[] = [...json, { name: fileName, width: data1.width, height: data1.height, thumbnail_height: data2.height, thumbnail_width: data2.width, format: 'webp' }];
-  await writeFile(json_file, JSON.stringify(newJSON), 'utf-8');
+  // only upload image if it hasn't been already uploaded
+  
+  if (!(await imageExists(hash))) {
+    const fileName = uuidv4();
+    // get metadata
+    // convert image
+    const data1 = await sharp(binaryString)
+      .webp({ quality: 90 })
+      .toFile(full_size + fileName + ".webp");
+    // create a thumbnail
+    const data2 = await sharp(binaryString)
+      .resize({ width: 1920, withoutEnlargement: true })
+      .webp({ quality: 60 })
+      .toFile(thumbnails + fileName + ".webp");
+    // add file to file index
+    const json = await getJSON();
+    const newJSON = [...json, { name: fileName, width: data1.width, height: data1.height, thumbnail_height: data2.height, thumbnail_width: data2.width, format: 'webp' as 'webp', hash: hash }];
+    await writeJSON(newJSON);
+  }
 }
 
 export async function deleteImage(id: string) {
   // remove image from json
-  const json: ImageFile[] = JSON.parse(await readFile(json_file, "utf8"));
+  const json = await getJSON();
   const image = json.find(item => item.name.startsWith(id));
 
   // only if image exists remove it
   if (image) {
     // remove json
     const filteredJSON = json.filter(item => !item.name.startsWith(id));
-    await writeFile(json_file, JSON.stringify(filteredJSON), 'utf-8');
+    await writeJSON(filteredJSON);
     // remove image files
     await rm(full_size + image.name + '.' + image.format);
     await rm(thumbnails + image.name + '.' + image.format);
@@ -42,7 +48,7 @@ export async function deleteImage(id: string) {
 // Creates the files/folders if they do not already exist
 export async function createFiles() {
   if (!(await access_wrapper(json_file))) {
-    await writeFile(json_file, JSON.stringify([]), 'utf-8');
+    writeJSON([]);
   }
   if (!(await access_wrapper(full_size))) {
     await mkdir(full_size, { recursive: true });
@@ -56,6 +62,24 @@ function access_wrapper(file: string) {
   return access(file).then(() => true, () => false);
 }
 
+function hashBuffer(buffer: Buffer) {
+  return hash('sha256', buffer);
+}
+
+async function imageExists(hash: string) {
+  const json: ImageFile[] = await getJSON();
+
+  return json.some(image => image.hash == hash);
+}
+
+async function getJSON(): Promise<ImageFile[]> {
+  return JSON.parse(await readFile(json_file, "utf8"));
+}
+
+async function writeJSON(data: ImageFile[]) {
+  await writeFile(json_file, JSON.stringify(data), 'utf-8');
+}
+
 // CONSTANTS AND INTERFACES
 export const thumbnails = process.env.IMAGE_FILES + "thumbnails/";
 export const full_size = process.env.IMAGE_FILES + "full_size/";
@@ -67,5 +91,6 @@ export interface ImageFile {
   height: number | undefined;
   thumbnail_width: number | undefined;
   thumbnail_height: number | undefined;
-  format: 'webp' | 'jpg'
+  format: 'webp' | 'jpg',
+  hash: string // hash of the original binary string
 };
