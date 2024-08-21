684a9824e2d73cf6a79fd7558a69759721ebc9da
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-08-21 18:30:47 +0200

    pdf display something something

--- pages/cv.vue
+++ pages/cv.vue
@@ -16,23 +16,29 @@ onMounted(() => {
   if (pdf) {
     for (let i = 1; i <= pages.value; i++) {
       pdf.getPage(i).then(page => {
-        const viewport = page.getViewport({ scale: 2 });
+        const scale = 1.5;
+        const viewport = page.getViewport({ scale: scale, });
+        // Support HiDPI-screens.
+        const outputScale = window.devicePixelRatio || 1;
 
-        // Prepare canvas using PDF page dimensions
         const canvas = document.getElementById('canvas-' + i) as HTMLCanvasElement;
         const context = canvas.getContext('2d')!;
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
 
-        // Render PDF page into canvas context
+        canvas.width = Math.floor(viewport.width * outputScale);
+        canvas.height = Math.floor(viewport.height * outputScale);
+        canvas.style.width = Math.floor(viewport.width) + "px";
+        canvas.style.height = Math.floor(viewport.height) + "px";
+
+        const transform = outputScale !== 1
+          ? [outputScale, 0, 0, outputScale, 0, 0]
+          : undefined;
+
         const renderContext = {
           canvasContext: context,
+          transform: transform,
           viewport: viewport
         };
-        const renderTask = page.render(renderContext);
-        renderTask.promise.then(function () {
-          console.log('Page rendered');
-        });
+        page.render(renderContext);
       });
     }
   }
@@ -48,19 +54,19 @@ onMounted(() => {
 </template>
 
 <style scoped lang="scss">
-  .container {
-    display: flex;
-    justify-content: center;
-  }
+.container {
+  display: flex;
+  justify-content: center;
+}
 
-  .inner-container {
-    display: flex;
-    flex-wrap: nowrap;
-    flex-direction: column;
-    width: 80%;
+.inner-container {
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  width: 80%;
 
-    @media screen and (max-width: 768px) {
-      width: 100%;
-    }
+  @media screen and (max-width: 768px) {
+    width: 100%;
   }
+}
 </style>
