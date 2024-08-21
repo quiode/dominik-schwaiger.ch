4b025599f98c5790887fe51a202712cd5d531409
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-08-21 18:59:00 +0200

    cv finished

--- pages/cv.vue
+++ pages/cv.vue
@@ -3,6 +3,7 @@ import * as pdfjsLib from 'pdfjs-dist';
 
 let pages = ref(0);
 let pdf = null as pdfjsLib.PDFDocumentProxy | null;
+let renders = [] as (Promise<any> | null)[];
 
 if (import.meta.client) {
   pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.mjs`
@@ -10,39 +11,50 @@ if (import.meta.client) {
 
   pdf = await pdfLoadingTask.promise;
   pages.value = pdf.numPages;
+
+  for (let index = 0; index <= pages.value; index++) {
+    renders.push(null);
+  }
 }
 
 onMounted(() => {
+  renderPages();
+
+  window.addEventListener('resize', renderPages);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', renderPages);
+});
+
+const renderPages = () => {
   if (pdf) {
     for (let i = 1; i <= pages.value; i++) {
-      pdf.getPage(i).then(page => {
-        const scale = 1.5;
-        const viewport = page.getViewport({ scale: scale, });
-        // Support HiDPI-screens.
-        const outputScale = window.devicePixelRatio || 1;
+      pdf.getPage(i).then(async page => {
+        const desiredWidth = window.screen.width <= 768 ? window.screen.width : Math.round(window.screen.width * 0.8);
+        let viewport = page.getViewport({ scale: 1, });
+        const scale = desiredWidth / viewport.width;
+        viewport = page.getViewport({ scale: scale, });
 
+        // Prepare canvas using PDF page dimensions
         const canvas = document.getElementById('canvas-' + i) as HTMLCanvasElement;
         const context = canvas.getContext('2d')!;
-
-        canvas.width = Math.floor(viewport.width * outputScale);
-        canvas.height = Math.floor(viewport.height * outputScale);
-        canvas.style.width = Math.floor(viewport.width) + "px";
-        canvas.style.height = Math.floor(viewport.height) + "px";
-
-        const transform = outputScale !== 1
-          ? [outputScale, 0, 0, outputScale, 0, 0]
-          : undefined;
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
 
         const renderContext = {
           canvasContext: context,
-          transform: transform,
           viewport: viewport
         };
+
+        if (renders[i]) {
+          await renders[i];
+        }
         page.render(renderContext);
       });
     }
   }
-});
+};
 </script>
 
 <template>
@@ -54,6 +66,8 @@ onMounted(() => {
 </template>
 
 <style scoped lang="scss">
+@import '../assets/colors.scss';
+
 .container {
   display: flex;
   justify-content: center;
@@ -63,10 +77,21 @@ onMounted(() => {
   display: flex;
   flex-wrap: nowrap;
   flex-direction: column;
+  gap: 1em;
   width: 80%;
 
   @media screen and (max-width: 768px) {
     width: 100%;
+  }
+}
+
+canvas {
+  border-radius: 10px;
+  border: 3px $primary-color solid;
+  transition: box-shadow 0.25s;
+
+  &:hover {
+    box-shadow: white 0px 0px 14px;
   }
 }
 </style>
