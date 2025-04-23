9524d8d58bba6e1c0617940c315cdb812b9146ee
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-04-23 16:34:40 +0200

    add push script

--- shell.nix
+++ shell.nix
@@ -14,6 +14,7 @@ mkShell {
   shellHook = ''
     export FILE_MOUNT="${builtins.toString ./.}/images/temp"
     export IMAGE_FILES="${builtins.toString ./.}/images"
+    alias push="git switch main && git merge dev --no-ff --no-edit && git switch dev"
     pnpm install --shamefully-hoist
   '';
 }
