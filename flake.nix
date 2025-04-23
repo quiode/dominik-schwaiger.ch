1697cb428c195f9d793bed3979a6eb7f8855b07e
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-04-23 15:37:44 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "dominik-schwaiger.ch dev environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    systems.url = "github:nix-systems/default-linux";
+  };
+
+  outputs =
+    inputs:
+    let
+      eachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
+      pkgsFor = inputs.nixpkgs.legacyPackages;
+    in
+    {
+      devShells = eachSystem (system: {
+        default = pkgsFor.${system}.callPackage ./shell.nix { };
+      });
+    };
+}
---
+++ shell.nix
@@ -0,0 +1,18 @@
+{
+  mkShell,
+  pnpm,
+  nodejs_20,
+}:
+mkShell {
+  name = "dominik-schwaiger.ch dev environment";
+
+  packages = [
+    pnpm
+    nodejs_20
+  ];
+
+  shellHook = ''
+    pnpm install --shamefully-hoist
+    pnpm run dev
+  '';
+}
